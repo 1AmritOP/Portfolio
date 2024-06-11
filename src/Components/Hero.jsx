057ee90ffaf734fc6f12c518e5713eba40f04e88
Amrit Kumar <amrit24680gg@gmail.com>
2024-06-11 22:37:00 +0530

    fix stupid things

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,50 +3,34 @@ import HeroImg from '../assets/HeroImg.png'
 import { MdWavingHand } from 'react-icons/md'
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react"
-import { SplitText } from "gsap-trial/SplitText";
 
 
 
 
 const Hero = () => {
-  gsap.registerPlugin(SplitText)
-  useGSAP(()=>{
-    const split = new SplitText(".slp",{type: "chars"})
 
-    gsap.from(split.chars,{
-        duration:1,
-        y:100,
-        autoAlpha:0,
-        stagger:0.5,
-    })
-    
-},[])
-
-  // useGSAP(() => {
-  //   const tl=gsap.timeline()
-  //   tl.from(".left p,.left h1",{
-  //       x:-200,
-  //       delay: 1,
-  //       duration:0.5,
-  //       opacity:0,
-  //       stagger: 0.4,
-  //   },"same")
-  //   tl.from(".right img",{
-  //     x:200,
-  //     delay: 1,
-  //     duration:1,
-  //     opacity:0,
-  //   },"same")
-  // },[]);
+  useGSAP(() => {
+    const tl=gsap.timeline()
+    tl.from(".left p,.left h1",{
+        x:-200,
+        delay: 1,
+        duration:0.5,
+        opacity:0,
+        stagger: 0.4,
+    },"same")
+    tl.from(".right img",{
+      x:200,
+      delay: 1,
+      duration:1,
+      opacity:0,
+    },"same")
+  },[]);
 
 
   return (
     <>
         <div className="hero mt-16 w-full min-h-[calc(100vh-64px)]   flex items-center justify-between overflow-hidden pl-[7vw] pr-[7vw] flex-wrap max-md:justify-center">
           <div className="left">
-            <div className="slp text-6xl">
-              Amrit
-            </div>
             <p className='  font-bold text-lg flex items-center'>Hello there! <MdWavingHand className=' text-[#EFAF33] ' /> </p>
             <h1 className='   font-bold text-[7vw] max-md:text-[18vw] max-md:text-5xl  leading-10'>I am <span>Amrit</span></h1>
             <h1 className='  font-bold text-[7vw] max-md:text-[18vw] max-md:text-5xl  '> 
